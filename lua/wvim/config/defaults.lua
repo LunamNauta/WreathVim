5d2913e4d22993f708ae68d3cf8e1c8fb9da4e66
LunamNauta <ayden.kranz@gmail.com>
2024-10-14 19:34:53 -0400

    Upload from 'UploadConfig' (14-Oct-24 7:34:53 PM)

--- lua/wvim/config/defaults.lua
+++ lua/wvim/config/defaults.lua
@@ -3,14 +3,15 @@ wvim.colorscheme = "catppuccin"
 wvim.is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win32unix") == 1
 
 wvim.languages = {}
-wvim.languages.treesitter = {"cpp", "c", "lua", "python", "rust"}
-wvim.languages.mason_lspconfig = {"clangd", "lua_ls", "pylsp", "rust_analyzer"}
+wvim.languages.treesitter = {"cpp", "c", "lua", "python", "rust", "javascript"}
+wvim.languages.mason_lspconfig = {"clangd", "lua_ls", "pylsp", "rust_analyzer", "ts_ls"}
 wvim.languages.mason_lspconfig_opts = {
     --If on windows, mason-lspconfig's clangd cannot find MSYS2 headers
     --This 'cmd' is needed in order to locate headers in my build system
     ["clangd"] = {cmd = {"C:\\msys64\\ucrt64\\bin\\clangd.exe"}},
     ["lua_ls"] = {settings = {Lua = {diagnostics = {globals = {"vim"}}}}},
     ["pylsp"] = {},
-    ["rust_analyzer"] = {}
+    ["rust_analyzer"] = {},
+    ["ts_ls"] = {}
 }
 return wvim
