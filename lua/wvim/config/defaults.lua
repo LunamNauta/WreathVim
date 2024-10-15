abdfeb258609a67307dabc83339eb741f7f30516
= <ayden.kranz@gmail.com>
2024-10-15 14:19:57 -0400

    Upload from 'UploadConfig' (10/15/2024 2:19:55 PM)

--- lua/wvim/config/defaults.lua
+++ lua/wvim/config/defaults.lua
@@ -3,8 +3,8 @@ wvim.colorscheme = "catppuccin"
 wvim.is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win32unix") == 1
 
 wvim.languages = {}
-wvim.languages.treesitter = {"cpp", "c", "lua", "python", "rust", "javascript"}
-wvim.languages.mason_lspconfig = {"clangd", "lua_ls", "pylsp", "rust_analyzer", "ts_ls"}
+wvim.languages.treesitter = {"cpp", "c", "lua", "python", "rust", "javascript", "html"}
+wvim.languages.mason_lspconfig = {"clangd", "lua_ls", "pylsp", "rust_analyzer", "ts_ls", "html"}
 wvim.languages.mason_lspconfig_opts = {
     --If on windows, mason-lspconfig's clangd cannot find MSYS2 headers
     --This 'cmd' is needed in order to locate headers in my build system
