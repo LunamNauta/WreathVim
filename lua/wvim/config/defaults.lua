f26e4e1d7125942d91829b8e10d611276ad7a51a
= <ayden.kranz@gmail.com>
2024-10-15 17:01:29 -0400

    Upload from 'UploadConfig' (Tue 15 Oct 2024 05:01:29 PM EDT)

--- lua/wvim/config/defaults.lua
+++ lua/wvim/config/defaults.lua
@@ -8,7 +8,9 @@ wvim.languages.mason_lspconfig = {"clangd", "lua_ls", "pylsp", "rust_analyzer", 
 wvim.languages.mason_lspconfig_opts = {
     --If on windows, mason-lspconfig's clangd cannot find MSYS2 headers
     --This 'cmd' is needed in order to locate headers in my build system
-    ["clangd"] = {cmd = {"C:\\msys64\\ucrt64\\bin\\clangd.exe"}},
+    ["clangd"] = {
+        cmd = wvim.is_windows and {"C:\\msys64\\ucrt64\\bin\\clangd.exe"} or nil
+    },
     ["lua_ls"] = {settings = {Lua = {diagnostics = {globals = {"vim"}}}}},
     ["pylsp"] = {},
     ["rust_analyzer"] = {},
